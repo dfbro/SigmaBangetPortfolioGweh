cc94f7a64201f11cfe4f7410f1712055e2551be6
Elang Syadewa <elangdimasgaming@gmail.com>
2026-03-15 08:06:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1871293026-71172",
+  "appId": "1:96679641856:web:2d1742832e672457d969e5",
+  "apiKey": "AIzaSyC3xAFwKR-YCaXbu3vKp6o1bGWb0S_xzoA",
+  "authDomain": "studio-1871293026-71172.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "96679641856"
+};
